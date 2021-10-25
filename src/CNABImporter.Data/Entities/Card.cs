9ad780c84c51d161064a9b0050ffc4d35330ffea
Evandre Silva <47608734+evandresilva@users.noreply.github.com>
2021-10-25 13:08:31 +0100

    add: create an entity for card informations

---
+++ src/CNABImporter.Data/Entities/Card.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CNABImporter.Data.Entities
+{
+    public class Card : BaseEntity
+    {
+        [MaxLength(12)]
+        public string CardNumber { get; set; }
+    }
+}
