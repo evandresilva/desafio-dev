cfadad27f318d784bd54e34170c15d270936c2ae
Evandre Silva <47608734+evandresilva@users.noreply.github.com>
2021-10-25 13:08:45 +0100

    create an entity for clients

---
+++ src/CNABImporter.Data/Entities/Client.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CNABImporter.Data.Entities
+{
+    public class Client: BaseEntity
+    {
+        [MaxLength(11)]
+        public string CPF { get; set; }
+    }
+}
