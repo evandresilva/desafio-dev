e07b7a2f6d3334018af7f17211b8893f334a1811
Evandre Silva <47608734+evandresilva@users.noreply.github.com>
2021-10-25 13:09:14 +0100

    add: create an entity for merchants

---
+++ src/CNABImporter.Data/Entities/Merchant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CNABImporter.Data.Entities
+{
+    public class Merchant: BaseEntity
+    {
+        [MaxLength(19)]
+        public string Name { get; set; }
+        [MaxLength(14)]
+        public string OwnerName { get; set; }
+    }
+}
