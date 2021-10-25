ef285d2628f869164f0e6b79c703a6d8019132b4
Evandre Silva <47608734+evandresilva@users.noreply.github.com>
2021-10-25 13:11:57 +0100

    add: add configuration to seed default values for transations nature

---
+++ src/CNABImporter.Data/Configurations/TransactionNatureConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using CNABImporter.Data.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CNABImporter.Data.Configurations
+{
+    public class TransactionNatureConfiguration
+    {
+        public void Configure(EntityTypeBuilder<TransactionNature> builder)
+        {
+            builder.HasData(new TransactionNature
+            {
+                Id = 1,
+                Description = "Incoming",
+            },
+            new TransactionNature
+            {
+                Id = 2,
+                Description = "Outcoming"
+            }
+          );
+        }
+    }
+}
