b2b7116bc40c9f6bc25d3cd9a765d799ec014ce0
Evandre Silva <47608734+evandresilva@users.noreply.github.com>
2021-10-25 13:11:05 +0100

    add: create an entity to save all transactions type

---
+++ src/CNABImporter.Data/Entities/TransactionType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CNABImporter.Data.Entities
+{
+    public class TransactionType: BaseEntity
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public new int Id { get; set; }
+        [MaxLength(16)]
+        public string Description { get; set; }
+        public int TransactionNatureId { get; set; }
+
+        [ForeignKey("TransactionNatureId")]
+        public TransactionNature TransactionNature { get; set; }
+    }
+}
