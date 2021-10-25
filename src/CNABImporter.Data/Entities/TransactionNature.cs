631f9b62fa7f2c924713cfb167b222ba298b9a81
Evandre Silva <47608734+evandresilva@users.noreply.github.com>
2021-10-25 13:09:49 +0100

    add: create an entity to save the nature of transaction

---
+++ src/CNABImporter.Data/Entities/TransactionNature.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace CNABImporter.Data.Entities
+{
+    public class TransactionNature
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+        [MaxLength(16)]
+        public string Description { get; set; }
+    }
+}
