0641d5170785b5d52c2cf86fd37627fd03660aff
Evandre Silva <47608734+evandresilva@users.noreply.github.com>
2021-10-25 13:07:56 +0100

    add: create a base entity for generic fields

---
+++ src/CNABImporter.Data/Entities/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CNABImporter.Data.Entities
+{
+    public class BaseEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; } = null;
+        public DateTime? DeletedAt { get; set; } = null;
+    }
+}
