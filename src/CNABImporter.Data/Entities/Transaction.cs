236e95ef36cf7206e4036d95eb0e8e26055d0758
Evandre Silva <47608734+evandresilva@users.noreply.github.com>
2021-10-25 13:10:30 +0100

    add: create an entity to save all transactions

---
+++ src/CNABImporter.Data/Entities/Transaction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace CNABImporter.Data.Entities
+{
+    public class Transaction : BaseEntity
+    {
+        [Column(TypeName = "money")]
+        [MaxLength(10)]
+        public decimal Amount { get; set; }
+        public Guid CardId { get; set; }
+        public Guid ClientId { get; set; }
+        public DateTime TransactedAt { get; set; }
+        public Guid MerchantId { get; set; }
+        public Guid TransactionTypeId { get; set; }
+
+        [ForeignKey("MerchantId")]
+        public Merchant Merchant { get; set; }
+        [ForeignKey("TransactionTypeId")]
+        public TransactionType TransactionType { get; set; }
+        [ForeignKey("CardId")]
+        public Card Card { get; set; }
+        [ForeignKey("ClientId")]
+        public Client Client { get; set; }
+    }
+}
