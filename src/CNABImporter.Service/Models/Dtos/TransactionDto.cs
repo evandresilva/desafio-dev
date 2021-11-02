ad0e0830e7fab9db3d799a56ee02bf959fde99a6
Evandre Silva <47608734+evandresilva@users.noreply.github.com>
2021-11-02 16:20:25 +0100

    add: add DTO for transactions

---
+++ src/CNABImporter.Service/Models/Dtos/TransactionDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CNABImporter.Service.Dtos
+{
+    public class TransactionDto
+    {
+       public Guid Id { get; set; }
+        public decimal Amount { get; set; }
+        public Guid CardId { get; set; }
+        public Guid ClientId { get; set; }
+        public DateTime TransactedAt { get; set; }
+        public Guid MerchantId { get; set; }
+        public Guid TransactionTypeId { get; set; }
+        public string Merchant { get; set; }
+        public string TransactionType { get; set; }
+        public string Card { get; set; }
+        public string Client { get; set; }
+    }
+}
