9920446b37050c2c7525896a2f28230d38698a3f
Evandre Silva <47608734+evandresilva@users.noreply.github.com>
2021-11-02 16:19:50 +0100

    add: add interface for transaction service

---
+++ src/CNABImporter.Service/Interfaces/ITransactionService.cs
@@ -0,0 +1,14 @@
+﻿using CNABImporter.Models.Service.Dtos;
+using CNABImporter.Service.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CNABImporter.Service.Interfaces
+{
+    public interface ITransactionService
+    {
+        public TransactionImportedRowDto ParseRowFromFile(string row);
+        public AppResult AddTransaction(string row);
+    }
+}
