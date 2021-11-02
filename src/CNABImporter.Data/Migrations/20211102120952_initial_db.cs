d536baa64918a078c735a6d3596bfb28b3532ebf
Evandre Silva <47608734+evandresilva@users.noreply.github.com>
2021-11-02 16:15:50 +0100

    add: adding migration and create database

---
+++ src/CNABImporter.Data/Migrations/20211102120952_initial_db.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CNABImporter.Data.Migrations
+{
+    public partial class initial_db : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cards",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CardNumber = table.Column<string>(type: "nvarchar(12)", maxLength: 12, nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cards", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CPF = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Merchants",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(19)", maxLength: 19, nullable: true),
+                    OwnerName = table.Column<string>(type: "nvarchar(14)", maxLength: 14, nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Merchants", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TransactionNatures",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(16)", maxLength: 16, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TransactionNatures", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TransactionTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(32)", maxLength: 32, nullable: true),
+                    TransactionNatureId = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TransactionTypes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TransactionTypes_TransactionNatures_TransactionNatureId",
+                        column: x => x.TransactionNatureId,
+                        principalTable: "TransactionNatures",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Amount = table.Column<decimal>(type: "money", maxLength: 10, nullable: false),
+                    CardId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ClientId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TransactedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    MerchantId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TransactionTypeId = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Transactions_Cards_CardId",
+                        column: x => x.CardId,
+                        principalTable: "Cards",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Transactions_Clients_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Clients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Transactions_Merchants_MerchantId",
+                        column: x => x.MerchantId,
+                        principalTable: "Merchants",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Transactions_TransactionTypes_TransactionTypeId",
+                        column: x => x.TransactionTypeId,
+                        principalTable: "TransactionTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "TransactionNatures",
+                columns: new[] { "Id", "Description" },
+                values: new object[] { 1, "Entrada" });
+
+            migrationBuilder.InsertData(
+                table: "TransactionNatures",
+                columns: new[] { "Id", "Description" },
+                values: new object[] { 2, "Saída" });
+
+            migrationBuilder.InsertData(
+                table: "TransactionTypes",
+                columns: new[] { "Id", "CreatedAt", "DeletedAt", "Description", "TransactionNatureId", "UpdatedAt" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2021, 11, 2, 12, 9, 52, 188, DateTimeKind.Utc).AddTicks(6498), null, "Débito", 1, null },
+                    { 4, new DateTime(2021, 11, 2, 12, 9, 52, 188, DateTimeKind.Utc).AddTicks(7957), null, "Crédito", 1, null },
+                    { 5, new DateTime(2021, 11, 2, 12, 9, 52, 188, DateTimeKind.Utc).AddTicks(7959), null, "Recebimento Empréstimo", 1, null },
+                    { 6, new DateTime(2021, 11, 2, 12, 9, 52, 188, DateTimeKind.Utc).AddTicks(7960), null, "Vendas", 1, null },
+                    { 7, new DateTime(2021, 11, 2, 12, 9, 52, 188, DateTimeKind.Utc).AddTicks(7962), null, "Recebimento TED", 1, null },
+                    { 8, new DateTime(2021, 11, 2, 12, 9, 52, 188, DateTimeKind.Utc).AddTicks(7963), null, "Recebimento DOC", 1, null },
+                    { 2, new DateTime(2021, 11, 2, 12, 9, 52, 188, DateTimeKind.Utc).AddTicks(7917), null, "Boleto", 2, null },
+                    { 3, new DateTime(2021, 11, 2, 12, 9, 52, 188, DateTimeKind.Utc).AddTicks(7955), null, "Financiamento", 2, null },
+                    { 9, new DateTime(2021, 11, 2, 12, 9, 52, 188, DateTimeKind.Utc).AddTicks(7965), null, "Aluguel", 2, null }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_CardId",
+                table: "Transactions",
+                column: "CardId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_ClientId",
+                table: "Transactions",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_MerchantId",
+                table: "Transactions",
+                column: "MerchantId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_TransactionTypeId",
+                table: "Transactions",
+                column: "TransactionTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TransactionTypes_TransactionNatureId",
+                table: "TransactionTypes",
+                column: "TransactionNatureId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transactions");
+
+            migrationBuilder.DropTable(
+                name: "Cards");
+
+            migrationBuilder.DropTable(
+                name: "Clients");
+
+            migrationBuilder.DropTable(
+                name: "Merchants");
+
+            migrationBuilder.DropTable(
+                name: "TransactionTypes");
+
+            migrationBuilder.DropTable(
+                name: "TransactionNatures");
+        }
+    }
+}
