ecd74c7c9e70fcfa5654bf1444fbd2c834b13f20
Evandre Silva <47608734+evandresilva@users.noreply.github.com>
2021-11-02 16:21:00 +0100

    add: add Dto for result of transactions int CNAB file

---
+++ src/CNABImporter.Service/Models/Dtos/TransactionImportedRowDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CNABImporter.Models.Service.Dtos
+{
+    public class TransactionImportedRowDto
+    {
+        public int TransactionTypeId { get; set; }
+        public DateTime TransactedAt { get; set; }
+        public decimal Amount { get; set; }
+        public string CPF { get; set; }
+        public string CardNumber { get; set; }
+        public string StoreName { get; set; }
+        public string StoreOwnerName { get; set; }
+    }
+}
